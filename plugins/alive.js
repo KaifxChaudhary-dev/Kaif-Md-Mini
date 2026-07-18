e91598fa4be52f82f250646db87cdaa1eaecc22d
Kaif Chaudhary <kaifchgee@gmail.com>
2026-07-18 18:43:02 +0500

    Update newsletter JID in alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -51,7 +51,7 @@ cmd({
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
+                    newsletterJid: '120363410230117027@newsletter',
                     newsletterName: 'ᴀʀꜱʟᴀɴ-ᴍᴅ',
                     serverMessageId: 143
                 }
